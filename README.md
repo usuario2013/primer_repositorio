b13814e0d10380af3860db7eba5dbefbb7303840
Ignacio Aular <ignacio_345@hotmail.com>
2013-02-11 06:35:03 -0430

    Mi primer commit IUTY 2013

---
+++ README.md
@@ -0,0 +1,1 @@
+Mostrando como usar un Sistema de Control de Versiones Descentralizado
---
+++ hola_mundo.c
@@ -0,0 +1,16 @@
+// Programa: hola_mundo.c
+// Autor: Ignacio Aular
+// Version: 1.0
+// Fecha: 11/02/2013
+// Hora: 06:15 a.m.
+// Descripcion: muestra una cadena de texto por la salida estandar.
+
+#include <stdio.h>
+
+int main(void)
+{
+   printf("Hola Mundo remoto desde GitHub.com\n");
+   return 0;
+}
+
+
